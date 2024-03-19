1a9abfa1baa8110241b35e79c1164bac1d6e498f
MirandaTse02 <114991502+MirandaTse02@users.noreply.github.com>
2024-03-19 13:40:33 +0800

    Added Room Info Page

--- frontend/Script/roomInfo.js
+++ frontend/Script/roomInfo.js
@@ -1,1 +1,6 @@
-window.onload = alert(localStorage.getItem("Room"))
+window.onload = function() {
+    var roomName = localStorage.getItem("Room");
+    var room = document.getElementById("roomName");
+    
+    room.innerText = roomName;
+}
