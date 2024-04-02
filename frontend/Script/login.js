1e1dcc166489f85b7f43660bf4acdd663ea73c5c
MirandaTse02 <114991502+MirandaTse02@users.noreply.github.com>
2024-04-02 14:08:39 +0800

    Update login.js

--- frontend/Script/login.js
+++ frontend/Script/login.js
@@ -1,19 +1,19 @@
 function login() {
     // get username
-	var username = document.getElementById('id');
+	var userid = document.getElementById('id');
     
     // get password 
 	var pw = document.getElementById('password');
     
     // php request
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'login.php');
+    xhr.open('POST', 'login.php?id=' + userid);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            document.cookie = "username="+username;
+            document.cookie = "userID="+userid;
         }
     }
-    xhr.send('username='+username+'&password='+pw);
+    xhr.send('password='+pw);
     
 }
