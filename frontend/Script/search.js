2de5bbac1388e5aa21f7476a22f1e28a04cab1d7
MirandaTse02 <114991502+MirandaTse02@users.noreply.github.com>
2024-04-02 14:06:21 +0800

    Update search.js
    added event listener

--- frontend/Script/search.js
+++ frontend/Script/search.js
@@ -2,3 +2,18 @@ function info(room) {
     localStorage.setItem("Room", room);
     window.open("../Page/roomInfo.html", "_self")
 }
+
+// fetch and display all room
+function fetchRoom() {
+    
+}
+
+// event listener category on change
+document.getElementById('select').addEventListener('change', function(event) {
+    event.preventDefault();
+
+    var cate = document.getElementById('select').value;
+    alter(cate);
+
+    var xhr = new XMLHttpRequest();
+});
