d533253955d68d99d4fb346883d258964e6519ed
MirandaTse02 <114991502+MirandaTse02@users.noreply.github.com>
2024-04-02 14:30:31 +0800

    Update search.js

--- frontend/Script/search.js
+++ frontend/Script/search.js
@@ -5,7 +5,21 @@ function info(room) {
 
 // fetch and display all room
 function fetchRoom() {
-    
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'getRoom.php?category=none'); // php may change
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // response json
+            var response = JSON.parse(xhr.responseText);
+            var roomList = document.getElementById('room');
+            roomList.innerHTML = '';
+            response.forEach(function(item) {
+                // show room by category
+            });
+        }
+    };
+    xhr.send();
 }
 
 // event listener category on change
@@ -13,7 +27,15 @@ document.getElementById('select').addEventListener('change', function(event) {
     event.preventDefault();
 
     var cate = document.getElementById('select').value;
-    alter(cate);
 
     var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'getRoom.php?category=' + cate); // php may change
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // response json
+            var response = JSON.parse(xhr.responseText);
+        }
+    };
+    xhr.send();
 });
