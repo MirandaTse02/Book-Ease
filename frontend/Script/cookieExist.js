1d799c614e153adaaaeeb2a95f8bde1e5b0fb289
MirandaTse02 <114991502+MirandaTse02@users.noreply.github.com>
2024-04-02 13:39:58 +0800

    Added cookieExist.js

---
+++ frontend/Script/cookieExist.js
@@ -0,0 +1,23 @@
+function getCookie(name) {
+	var c = document.cookie;
+    var prefix = name + "=";
+    var begin = c.indexOf("; "+prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    
+    return "ok";
+}
+
+function cookieExist() {
+    // if no cookie, i.e. not login, redirect to login.html
+	var exist = getCookie("username");
+    
+    if (exist == null) {
+        // go to login.html
+		location.replace("/Page/login.html");
+    }
+}
+
+cookieExist();
