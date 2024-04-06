714bb2c81734e4e548be5a183afbe47b892daa28
MirandaTse02 <114991502+MirandaTse02@users.noreply.github.com>
2024-04-06 09:34:45 +0800

    Update roomInfo.js

--- frontend/Script/roomInfo.js
+++ frontend/Script/roomInfo.js
@@ -4,3 +4,22 @@ window.onload = function() {
     
     room.innerText = roomName;
 }
+
+// get room info from db
+function getRoomInfo() {
+    var roomName = localStorage.getItem("Room");
+    
+    var xhr = nee XMLHttpRequest();
+    xhr.open('GET', 'getRoomInfo.php?roomName='+roomName);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // response json
+			var response = JSON.parse(xhr.responseText);
+            
+            // append info
+        }
+    }
+}
+
+getRoomInfo();
