63902a8582d9648ca0a69ad7af7ee82562a0f6e5
MirandaTse02 <114991502+MirandaTse02@users.noreply.github.com>
2024-04-09 16:05:50 +0800

    Update roomInfo.js

--- frontend/Script/roomInfo.js
+++ frontend/Script/roomInfo.js
@@ -9,15 +9,54 @@ window.onload = function() {
 function getRoomInfo() {
     var roomName = localStorage.getItem("Room");
     
-    var xhr = nee XMLHttpRequest();
+    var xhr = new XMLHttpRequest();
     xhr.open('GET', 'getRoomInfo.php?roomName='+roomName);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             // response json
 			var response = JSON.parse(xhr.responseText);
+
+            var divInfo = document.getElementById("info");
+            var divPhoto = document.getElementById("photo");
             
             // append info
+            // room photo
+            var roomImg = document.createElement("img");
+            roomImg.src = response.src; // check php/ json
+            roomImg.alt = "room photo";
+            roomImg.id = "roomPhoto";
+            divPhoto.appendChild(roomImg);
+
+            // location
+            var location = document.createElement("p");
+            location.id = "location";
+            location.className = "info";
+            location.innerHTML = "Location: " + response.location; // check php/ json
+            divInfo.appendChild(location);
+
+            // capacity
+            var capacity = document.createElement("p");
+            capacity.id = "capacity";
+            capacity.className = "info";
+            capacity.innerHTML = "Capacity: " + response.capacity; // check php/ json
+            divInfo.appendChild(capacity);
+
+            // Equipment
+            var equipText = document.createElement("p");
+            equipText.className = "info";
+            equipText.innerHTML = "Equipment:";
+            divInfo.appendChild(equipText);
+
+            var equipmentList = document.createElement("ul");
+            equipmentList.id = "equip";
+            equipmentList.className = "info";
+            response.equipment.forEach(function(equip) {
+                var li = document.createElement("li");
+                li.innerHTML = equip.name;
+                equipmentList.appendChild(li);
+            });
+            divInfo.appendChild(equipmentList);
         }
     }
 }
