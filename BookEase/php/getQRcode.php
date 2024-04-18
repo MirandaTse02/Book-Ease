c7c5fa71707517fff9386ef45b6d3f0cc3a1ef54
stella-hydrangea <cheryltamu@gmail.com>
2024-04-18 19:01:56 +0800

    add getQRcode.php

---
+++ BookEase/php/getQRcode.php
@@ -0,0 +1,27 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
+    $bookingID = $_GET['bookingID'];
+
+    $host = 'localhost';
+    $dbName = 'room_booking_app';
+    $username = 'root';
+    $DBpassword = '';
+    $conn = new mysqli($host, $username, $DBpassword, $dbName);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $stmt = $conn->prepare('SELECT pic FROM QRcode WHERE bookingID = ?');
+    $stmt->bind_param("s", $bookingID);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        echo json_encode($row);
+    }
+    else {
+        echo "no QRcode";
+    }
+?>
