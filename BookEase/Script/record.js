bc2703675efc83ffcbe05d69c8b463d9f427b54e
MirandaTse02 <114991502+MirandaTse02@users.noreply.github.com>
2024-04-19 16:26:06 +0800

    Fixed record.js

--- BookEase/Script/record.js
+++ BookEase/Script/record.js
@@ -24,7 +24,8 @@ function getRecord() {
              var record = document.getElementById("record");
              record.innerHTML ="<tr><th>Room</th><th>Date</th><th>Time</th><th>QR Code</th></tr>";
              var count = Object.keys(response).length;
-             if (count/7 == 1) {
+             alert(count);
+             if (count == 7) {
                 var row = "<tr>" +
                      "<td>" + response.roomID + "</td>" +
                      "<td>" + response.bookDate + "</td>" +
@@ -34,7 +35,7 @@ function getRecord() {
 
                   document.getElementById("record").innerHTML += row;
              } else {
-                for (var i = 0; i < count/7; i++) {
+                for (var i = 0; i < count; i++) {
                     var row = "<tr>" +
                      "<td>" + response[i].roomID + "</td>" +
                      "<td>" + response[i].bookDate + "</td>" +
