d09edb653493ee1eea83a1efd59f0cf9f19a423d
stella-hydrangea <cheryltamu@gmail.com>
2024-04-19 17:21:38 +0800

    add Sign out button and Welcome message

--- BookEase/Script/home.js
+++ BookEase/Script/home.js
@@ -11,3 +11,33 @@ function boldTitle(id) {
 
     target.style.fontWeight = "bold";
 }
+
+function getCookieUserName(name) {
+    var c = document.cookie;
+    var prefix = name + "=";
+    var begin = c.indexOf("; "+prefix);
+    if (begin == -1) {
+        begin = c.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    
+    return decodeURI(c.substring(begin + prefix.length));;
+}
+
+
+window.onload = function() {
+    var userid = getCookieUserName("userID");
+    var xhr = new XMLHttpRequest();
+    alert(userid);
+    xhr.open('GET', '../php/home.php?id='+userid);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+            console.log(response);
+            var msg = document.getElementById("welcome_msg");
+            msg.innerHTML = "Welcome! "+response.name;
+        }
+    }
+    xhr.send();
+}
