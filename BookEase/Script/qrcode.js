7f8f65cdef1ff495ec7b258c59c91ae32d463129
MirandaTse02 <114991502+MirandaTse02@users.noreply.github.com>
2024-04-20 09:08:49 +0800

    Fixed: can show QRcode

--- BookEase/Script/qrcode.js
+++ BookEase/Script/qrcode.js
@@ -6,17 +6,22 @@ function qr() {
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             var response = JSON.parse(xhr.responseText);
-            var QRCode = document.getElementById("qr");
-            var QRcodeImg = document.createElement("img");
-            // var room = document.getElementById("roomName");
-            // room.innerText = roomName;
-            QRcodeImg.src = "../php/pics/QRcode/" + response.pic; // check php/ json
-            QRcodeImg.alt = "QRcode";
-            QRcodeImg.id = "QRcode";
-            QRCode.appendChild(QRcodeImg);
+            setImg(response.pic);
+            
         }
     }
     xhr.send();
+}
+
+function setImg(pic) {
+    var QRCode = document.getElementById("qr");
+    var QRcodeImg = document.createElement("img");
+    // var room = document.getElementById("roomName");
+    // room.innerText = roomName;
+    QRcodeImg.src = '../php/pics/QRcode/'+ pic+'.png'; // check php/ json
+    QRcodeImg.alt = "QRcode failed to load";
+    QRcodeImg.id = "QRcode";
+    QRCode.appendChild(QRcodeImg);
 }
 
 qr();
